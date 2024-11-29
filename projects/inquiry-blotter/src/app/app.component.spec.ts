8b7aeb68370ca85a8f64e0ee6772223c5f66c8ec
zhigang.zhou <13120608736@163.com>
2024-11-29 22:35:09 +0800

    chore: change the project name

--- projects/inquiry-blotter/src/app/app.component.spec.ts
+++ projects/inquiry-blotter/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'InquiryBlotter' title`, () => {
+  it(`should have the 'Inquiry-blotter' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('InquiryBlotter');
+    expect(app.title).toEqual('Inquiry-blotter');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, InquiryBlotter');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Inquiry-blotter');
   });
 });
--- projects/inquiry-capture/src/app/app.component.spec.ts
+++ projects/inquiry-capture/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'InquiryCapture' title`, () => {
+  it(`should have the 'inquiry-capture' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('InquiryCapture');
+    expect(app.title).toEqual('inquiry-capture');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, InquiryCapture');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, inquiry-capture');
   });
 });
